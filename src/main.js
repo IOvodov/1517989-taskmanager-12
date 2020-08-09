4937001d9e2395e4d75cc49068e0f195ce9099d0
ovodov_ii <pain3007@mail.ru>
2020-08-09 19:41:17 +0300

    Реализовал показ оставшихся задач

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ import {generateRandomTask} from "./mock/task.js";
 import {generateTasksFilter} from "./mock/filter.js";
 import {render} from "./utils.js";
 
-const TASKS_COUNT = 8;
+const TASKS_COUNT = 25;
+const TASKS_COUNT_PER_STEP = 8;
 
 const tasks = new Array(TASKS_COUNT).fill().map(generateRandomTask);
 const tasksFilter = generateTasksFilter(tasks);
@@ -28,8 +29,27 @@ render(boardElement, createSortingTemplate(), `afterbegin`);
 
 render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 
-for (let i = 1; i < TASKS_COUNT; i++) {
+for (let i = 1; i < Math.min(tasks.length, TASKS_COUNT_PER_STEP); i++) {
   render(taskListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
-render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+if (tasks.length > TASKS_COUNT_PER_STEP) {
+  let renderedTaskCount = TASKS_COUNT_PER_STEP;
+
+  render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+
+  const loadMoreButton = document.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, (e) => {
+    e.preventDefault();
+    tasks
+      .slice(renderedTaskCount, renderedTaskCount + TASKS_COUNT_PER_STEP)
+      .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+
+    renderedTaskCount += TASKS_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
+  })
+}
